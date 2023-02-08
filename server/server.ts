94ac1f3b68953c7ab0c904290a5684129c8b1cc1
BhanuPratap-Singh-Naruka <pratapbhanu0187@gmail.com>
2023-02-08 09:47:53 +0530

    feat: Add react router for Routing

--- server/server.ts
+++ server/server.ts
@@ -7,11 +7,4 @@ const app = fastify();
 
 app.listen({ port: parseInt(process.env.PORT!) });
 
-const object = {
-  name: "Bhanu",
-  lastName: "Pratap",
-  age: 23,
-  name1: "Abhishek",
-  lastName1: "Mishra",
-  age1: 23,
-};
+
