a5f4a26f27946a31ffb2dceee1519987ffd3a157
BhanuPratap-Singh-Naruka <pratapbhanu0187@gmail.com>
2023-02-27 09:08:13 +0530

    feat: Added Reusable input card

--- client/src/pages/layouts/Authlayout.tsx
+++ client/src/pages/layouts/Authlayout.tsx
@@ -1,10 +1,8 @@
-import React from 'react'
-import {Outlet} from "react-router-dom"
+import React from "react";
+import { Outlet } from "react-router-dom";
 
 const Authlayout = () => {
-  return (
-    <Outlet />
-  )
-}
+  return <Outlet />;
+};
 
-export default Authlayout
+export default Authlayout;
