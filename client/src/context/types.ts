f29d11460b8f46e6df43eae51c255fa6c6eee1f1
Abhishek Mishra <abhisk.mhra@gmail.com>
2023-03-08 19:16:08 +0530

    update: AuthContext type with signup

--- client/src/context/types.ts
+++ client/src/context/types.ts
@@ -1,6 +1,10 @@
+import { UseMutationResult } from "@tanstack/react-query";
+import { AxiosResponse } from "axios";
 import { ReactNode } from "react";
 
-export type AuthContext = {};
+export type AuthContext = {
+  signup: UseMutationResult<AxiosResponse, unknown, User>;
+};
 
 export type AuthProviderProps = {
   children: ReactNode;
