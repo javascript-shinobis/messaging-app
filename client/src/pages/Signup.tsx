4ca10747fe251fbe62ff3ccc583af85b370e623b
BhanuPratap-Singh-Naruka <pratapbhanu0187@gmail.com>
2023-03-09 07:47:58 +0530

    style: Fromatted the code files

--- client/src/pages/Signup.tsx
+++ client/src/pages/Signup.tsx
@@ -4,10 +4,8 @@ import { Card } from "components/Card";
 import Input from "components/Input";
 
 const Signup = () => {
-  const usernameRef = useRef<HTMLInputElement>(null)
-  
+  const usernameRef = useRef<HTMLInputElement>(null);
 
- 
   return (
     <>
       <Card.Header>
@@ -16,7 +14,13 @@ const Signup = () => {
       <Card.Header>
         <form className="grid gris-cols-[auto,1fr] gap-x-3 gap-y-5 items-center justify-items-end">
           <label htmlFor="username">Username</label>
-          <Input type="text" id="username" pattern="\s*" required ref={usernameRef}/>
+          <Input
+            type="text"
+            id="username"
+            pattern="\s*"
+            required
+            ref={usernameRef}
+          />
         </form>
       </Card.Header>
     </>
--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,6 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
-import path from "path"
+import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -11,7 +11,7 @@ export default defineConfig({
       components: `${path.resolve(__dirname, "./src/components/")}`,
       public: `${path.resolve(__dirname, "./public/")}`,
       pages: `${path.resolve(__dirname, "./src/pages/")}`,
-      server: `${path.resolve(__dirname, "../server/")}`
-    }
-  }
+      server: `${path.resolve(__dirname, "../server/")}`,
+    },
+  },
 });
