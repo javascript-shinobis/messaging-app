d7d9abfee02fd4e0b47c7076973e7fb0e694e798
Abhishek Mishra <abhisk.mhra@gmail.com>
2023-03-09 09:33:17 +0530

    feat: Wrap ContextProvider into router

--- client/src/router.tsx
+++ client/src/router.tsx
@@ -1,16 +1,30 @@
-import { Children } from "react";
-import { createBrowserRouter } from "react-router-dom";
+import { createBrowserRouter, Outlet } from "react-router-dom";
 
+import { AuthProvider } from "./context/AuthContext";
 import Authlayout from "./pages/layouts/Authlayout";
 import Login from "./pages/Login";
 import Signup from "./pages/Signup";
 
 export const router = createBrowserRouter([
   {
-    element: <Authlayout />,
+    element: <ContextProvider />,
     children: [
-      { path: "login", element: <Login /> },
-      { path: "signup", element: <Signup /> },
+      {
+        element: <Authlayout />,
+
+        children: [
+          { path: "login", element: <Login /> },
+          { path: "signup", element: <Signup /> },
+        ],
+      },
     ],
   },
 ]);
+
+function ContextProvider() {
+  return (
+    <AuthProvider>
+      <Outlet />
+    </AuthProvider>
+  );
+}
