ddba93d23278732560a9c6f1ca61a35bb4c1aef4
BhanuPratap-Singh-Naruka <pratapbhanu0187@gmail.com>
2023-03-22 20:15:51 +0530

    feat: created login API

--- client/src/components/ActionButton/index.tsx
+++ client/src/components/ActionButton/index.tsx
@@ -10,7 +10,7 @@ const ActionButton = ({
   return (
     <>
       <button
-        className={`btn btn-xs sm:btn-sm md:btn-md lg:btn-md cursor-pointer text-white bg-cyan-600 hover:bg-cyan-700 w-full ${
+        className={`btn btn-xs sm:btn-sm md:btn-md lg:btn-md cursor-pointer text-white bg-cyan-600 hover:bg-cyan-700 w-full my-4 ${
           loading && "loading"
         }`}
         type={type}
