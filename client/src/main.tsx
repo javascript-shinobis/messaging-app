27fef68040ad0f2d18e4462abca96697828348d1
BhanuPratap-Singh-Naruka <pratapbhanu0187@gmail.com>
2023-03-23 09:29:12 +0530

    feat: configured eslint and prettier-eslint

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,17 +1,18 @@
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import React from "react";
-import ReactDOM from "react-dom/client";
-import { RouterProvider } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { RouterProvider } from 'react-router-dom';
 
-import "./index.css";
-import { router } from "./router";
+import './index.css';
+import { router } from './router';
 
 const queryClient = new QueryClient(); // creates a new queryClient and allows react query to work
 
-ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
       <RouterProvider router={router} />
     </QueryClientProvider>
+    P
   </React.StrictMode>
 );
