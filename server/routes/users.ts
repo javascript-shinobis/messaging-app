35269322454eb0ef485ce67b0cf1bad56a200bf6
Abhishek Mishra <abhisk.mhra@gmail.com>
2023-03-23 09:09:34 +0530

    feat`!`: wrap /signup route in server with try catch block for better error handlings

--- server/routes/users.ts
+++ server/routes/users.ts
@@ -10,22 +10,28 @@ export async function userRoutes(app: FastifyInstance) {
   app.post<{ Body: { id: string; name: string; image?: string } }>(
     "/signup",
     async (req, res) => {
-      const { id, name, image } = req.body;
-      if (id == null || id === "" || name == null || name === "") {
-        return res
-          .status(400)
-          .send({ errorMessage: `Please pass valid user signup parameters!` });
-      }
+      try {
+        const { id, name, image } = req.body;
+        if (id == null || id === "" || name == null || name === "") {
+          return res.status(400).send({
+            errorMessage: `Please pass valid user signup parameters!`,
+          });
+        }
 
-      //queryUser() allows us to query about our users
-      const existingUsers = await streamChat.queryUsers({ id });
-      if (existingUsers.users.length > 0) {
-        return res.status(400).send({ errorMessage: "User Id is taken." });
-      }
+        //queryUser() allows us to query about our users
+        const existingUsers = await streamChat.queryUsers({ id });
+        if (existingUsers.users.length > 0) {
+          return res.status(400).send({ errorMessage: "User Id is taken." });
+        }
 
-      // For creating or updating user we use upsertUser
-      const response = await streamChat.upsertUser({ id, name, image });
-      return response.users[id];
+        // For creating or updating user we use upsertUser
+        const response = await streamChat.upsertUser({ id, name, image });
+        return response.users[id];
+      } catch (error) {
+        return res.status(500).send({
+          errorMessage: `Unable to create a new user!`,
+        });
+      }
     }
   );
 }
