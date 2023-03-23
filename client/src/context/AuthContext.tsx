8317c7f8567735eda7b439707ba4057acaa63e06
Abhishek Mishra <abhisk.mhra@gmail.com>
2023-03-23 20:25:27 +0530

    feat: integrate login api route into AuthContext

--- client/src/context/AuthContext.tsx
+++ client/src/context/AuthContext.tsx
@@ -1,11 +1,10 @@
-import { useMutation } from "@tanstack/react-query";
-import axios from "axios";
-import { createContext } from "react";
-import { useContext } from "react";
-import toast from "react-hot-toast";
-import { useNavigate } from "react-router-dom";
+import { useMutation } from '@tanstack/react-query';
+import axios from 'axios';
+import { createContext, useMemo, useContext } from 'react';
+import toast from 'react-hot-toast';
+import { useNavigate } from 'react-router-dom';
 
-import { AuthContext, AuthProviderProps, User } from "./types";
+import { AuthContext, AuthProviderProps, User } from './types';
 
 const Context = createContext<AuthContext | null>(null);
 const SignupMethod = (navigation: (a: string) => void) =>
@@ -15,23 +14,40 @@ const SignupMethod = (navigation: (a: string) => void) =>
     },
     onSuccess: () => {
       toast.success(
-        "User has been created successfully. Please login to continue...!",
+        'User has been created successfully. Please login to continue...!',
         {
           duration: 3000,
         }
       );
       setTimeout(() => {
-        navigation("/login");
+        navigation('/login');
       }, 2000);
+    },
+  });
+
+const LoginMethod = (navigation: (a: string) => void) =>
+  useMutation({
+    mutationFn: (userName: string) => {
+      return axios.post(`${import.meta.env.VITE_SERVER_URL}/login`, userName);
+    },
+    onSuccess: () => {
+      toast.success('Login successful...!', {
+        duration: 2000,
+      });
+      setTimeout(() => {
+        navigation('/');
+      }, 1000);
     },
   });
 
 export const useAuth = () => useContext(Context) as AuthContext;
 
-export const AuthProvider = ({ children }: AuthProviderProps) => {
+export function AuthProvider({ children }: AuthProviderProps) {
   const navigate = useNavigate();
   const signup = SignupMethod(navigate);
-  const isSuccess = signup.isSuccess;
+  const login = LoginMethod(navigate);
+  const { isSuccess } = signup;
+  const isValidUser = login.isSuccess;
 
   // if(isSuccess){
   //
@@ -40,8 +56,13 @@ export const AuthProvider = ({ children }: AuthProviderProps) => {
   // console.log(signup)
 
   return (
-    <Context.Provider value={{ signup, isSuccess }}>
+    <Context.Provider
+      value={useMemo(
+        () => ({ signup, isSuccess, isValidUser, login }),
+        [signup, isSuccess, isValidUser, login]
+      )}
+    >
       {children}
     </Context.Provider>
   );
-};
+}
