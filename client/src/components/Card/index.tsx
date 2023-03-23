8373886b60e171bb5ecfb6b78481ef373ecc6c28
Abhishek Mishra <abhisk.mhra@gmail.com>
2023-03-23 10:12:41 +0530

    style: add a margin-top gap for card footer

--- client/src/components/Card/index.tsx
+++ client/src/components/Card/index.tsx
@@ -26,7 +26,7 @@ Card.Body = ({ children }: CardProps) => (
 );
 
 Card.Footer = ({ children }: CardProps) => (
-  <div className="  bg-white justify-center  rounded-lg flex gap-1 w-96 mx-auto">
+  <div className=" mt-2 bg-white justify-center  rounded-lg flex gap-1 w-96 mx-auto">
     {children}
   </div>
 );
